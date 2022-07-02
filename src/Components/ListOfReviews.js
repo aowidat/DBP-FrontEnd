d4a8abbf477cbf350b4d3ca880cf56b8c7f96aa3
Aziz <aziz.owidat@gmail.com>
2022-07-02 02:39:16 +0200

    Review fertig

--- src/Components/ListOfReviews.js
+++ src/Components/ListOfReviews.js
@@ -9,10 +9,18 @@ import Alert from "@mui/material/Alert";
 
 export default function ListOfReviews() {
   const [content, setContent] = useState("");
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(1);
   const [summary, setSummary] = useState("");
   const [user, setUser] = useState("");
   const [review, setReview] = useState(false);
+  const [allertMSG, setAllertMSG] = useState('');
+  const [sev, setSev] = useState();
+  const [conError, setConError] = useState('');
+  const [ratError, setRatError] = useState('');
+  const [sumError, setSumError] = useState('');
+  const [usrError, setUsrError] = useState('');
+  const min = 1;
+  const max = 6;
 
   let { parm1 } = useParams();
   const buildBody = () => {
@@ -29,7 +37,6 @@ export default function ListOfReviews() {
     });
   };
   const submit = () => {
-    console.log(buildBody());
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -37,101 +44,152 @@ export default function ListOfReviews() {
     };
     fetch("http://localhost:8080/review/addReview", requestOptions)
       .then((response) => response.json())
-      .then((data) => setReview(true));
+      .then((data) => {
+        if (data.status === 'success') {
+          setAllertMSG(data.status)
+          setSev('success');
+          setSumError('');
+          setConError('');
+          setUsrError('');
+          setRatError('');
+          setReview(true);
+        } else {
+          setAllertMSG(data.status);
+          setSev('error')
+          if (data.status === 'Error Rating') {
+            setRatError('Error Rating');
+            setSumError('');
+            setConError('');
+            setUsrError('');
+          } else if (data.status === 'Error Summery') {
+            setSumError('Error Summery');
+
+            setRatError('');
+            setConError('');
+            setUsrError('');
+          } else if (data.status === 'Error Content') {
+            setConError('Error Rating');
+
+            setRatError('');
+            setSumError('');
+            setUsrError('');
+          } else if (data.status === 'Error User') {
+            setUsrError('Error User');
+
+            setRatError('');
+            setSumError('');
+            setConError('');
+          } else {
+            setRatError('Error Rating');
+            setSumError('Error Summery');
+            setConError('Error Rating');
+            setUsrError('Error User');
+          }
+          setReview(true);
+        }
+      });
   };
   return (
     <>
-      {!review && (
-        <div
-          style={{
-            position: "absolute",
-            left: "50%",
-            top: "40%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          <Box
-            component="form"
-            sx={{
-              "& .MuiTextField-root": { m: 1, width: "25ch" },
-            }}
-            noValidate
-            autoComplete="off"
-          >
-            <div>
-              <TextField
-                required
-                id="content"
-                label="Content"
-                type="text"
-                value={content}
-                onChange={(e) => {
-                  setContent(e.target.value);
-                }}
-              />
-              <TextField
-                required
-                id="rating"
-                label="Rating"
-                type="number"
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                value={rating}
-                onChange={(e) => {
-                  setRating(e.target.value);
-                }}
-              />
-              <TextField
-                required
-                id="summary"
-                label="Summary"
-                type="text"
-                value={summary}
-                onChange={(e) => {
-                  setSummary(e.target.value);
-                }}
-              />
-              <TextField
-                disabled
-                id="product_id"
-                label="Product ID"
-                type="text"
-                value={parm1}
-              />
-              <TextField
-                required
-                id="user_name"
-                label="Costumer name"
-                type="text"
-                value={user}
-                onChange={(e) => {
-                  setUser(e.target.value);
-                }}
-              />
-              <br></br>
-              <br></br>
-              <Stack
-                direction="row"
-                spacing={2}
-                style={{
-                  position: "absolute",
-                  left: "25%",
-
-                  transform: "translate(-50%, -50%)",
-                }}
-              >
-                <Button onClick={submit}>Submit</Button>
-              </Stack>
-            </div>
-          </Box>
-        </div>
-      )}
       {review && (
-        <Alert variant="filled" severity="success">
-          This is a success alert — check it out!
+        <Alert variant="filled" severity={`${sev}`} onClose={() => { setReview(false) }}>
+          {`${allertMSG}`}
         </Alert>
       )}
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          top: "40%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              required
+              error={conError !== ''}
+              id="content"
+              label="Content"
+              type="text"
+              helperText="Not Empty"
+              value={content}
+              onChange={(e) => {
+                setContent(e.target.value);
+              }}
+              multiline
+            />
+            <TextField
+              required
+              error={ratError !== ''}
+              id="rating"
+              label="Rating"
+              type="number"
+              helperText="Numbers between 1 and 6"
+              value={rating}
+              onChange={(e) => {
+                let value = e.target.value;
+                if (value > max) value = max;
+                if (value < min) value = min;
+                setRating(value);
+              }}
+            />
+            <TextField
+              required
+              error={sumError !== ''}
+              id="summary"
+              label="Summary"
+              helperText="Not Empty"
+              type="text"
+              value={summary}
+              onChange={(e) => {
+                setSummary(e.target.value);
+              }}
+            />
+            <TextField
+              disabled
+              id="product_id"
+              label="Product ID"
+              helperText="can not be changed, is provied automaticly"
+              type="text"
+              value={parm1}
+            />
+            <TextField
+              required
+              error={usrError !== ''}
+              id="user_name"
+              label="Costumer name"
+              helperText="new User"
+              type="text"
+              value={user}
+              onChange={(e) => {
+                setUser(e.target.value);
+              }}
+            />
+            <br></br>
+            <br></br>
+            <Stack
+              direction="row"
+              spacing={2}
+              style={{
+                position: "absolute",
+                left: "25%",
+
+                transform: "translate(-50%, -50%)",
+              }}
+            >
+              <Button onClick={submit}>Submit</Button>
+            </Stack>
+          </div>
+        </Box>
+      </div>
     </>
   );
 }
