50a036333290005fdec31c7ad0412e32a555251c
Aziz <aziz.owidat@gmail.com>
2022-07-03 21:04:30 +0200

    a

--- src/Components/CategoriesTree.js
+++ src/Components/CategoriesTree.js
@@ -3,8 +3,6 @@ import TreeView from '@mui/lab/TreeView';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import TreeItem from '@mui/lab/TreeItem';
-import Button from '@mui/material/Button';
-import Box from '@mui/material/Box';
 
 
 function CategoriesTree() {
