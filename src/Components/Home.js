2bef315c1b8bfe157d6bd5d1623b8becea501e4d
madoamin <ma30zare@stud.uni-leipzig.de>
2022-07-03 09:46:04 +0200

    Update Home.js design and valitation tje Get Trolls

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import {Button} from "@mui/material";
+import * as React from "react";
+import TextField from "@mui/material/TextField";
 
 function Home() {
   const [id, setId] = useState("");
@@ -7,56 +10,204 @@ function Home() {
   const [sim, setSim] = useState("");
   const [path, setPath] = useState("");
   const [avgUser, setAvgUser] = useState(0);
+  const min = 0;
+  const max = 5;
+    const [isSelected, setIsSelected] = useState(true);
 
-  return (
-    <div className="p-3 text-center">
-      <Link to={`/user/${avgUser}`}>getTroll</Link>
-      <input
-        type="number"
-        value={avgUser}
-        onChange={(e) => setAvgUser(e.target.value)}
-      />
-      <br />
-      <Link to="/categoriestree">getCategoriesTree</Link>
-      <br />
-      <br />
-      <div className="p-3 text-center">
-        <Link to={`/product/byId/${id}`}>getProduct by ID</Link>
-        <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
-      </div>
-      <div className="p-3 text-center">
-        <Link to={`/product/bypattern/${pattern}`}>getProduct by Pattern</Link>
-        <input
-          type="text"
-          value={pattern}
-          onChange={(e) => setPattern(e.target.value)}
+    return (
+        <div>
+    <div  style={{
+      position: "absolute",
+      left: "39%",
+      top: "15%",
+      transform: "translate(-50%, -50%)",
+    }}
+    >
+        <Button
+            variant="contained"
+            href={`/user/${avgUser}`}
+        >
+            Get Trolls
+        </Button>&nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+      <div  style={{
+        position: "absolute",
+        left: "50%",
+        top: "15%",
+        transform: "translate(-50%, -50%)",
+      }}
+      >
+        <TextField
+            type="number"
+            inputProps={{ min, max }}
+            value={avgUser}
+            onChange={(e) => {
+                let value = parseInt(e.target.value, 10);
+                if (value >= max) value = max;
+                if (value < min) value = min;
+                setAvgUser(value);
+            }}
+            required
+            label="Number"
         />
-      </div>
-
-      <div className="p-3 text-center">
-        <Link to={`/product/allproduct/nnn`}>get all products</Link>
-      </div>
-
-      <div className="p-3 text-center">
-        <Link to={`/product/similars/${sim}`}>similar cheaper</Link>
-        <input
-          type="text"
-          value={sim}
-          onChange={(e) => setSim(e.target.value)}
+        </div>
+      <br />
+      <br />
+          <div  style={{
+            position: "absolute",
+            left: "39%",
+            top: "25%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/byId/${id}`}
+        >Product by ID
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "54%",
+            top: "25%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <TextField
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            required
+            label="Product ID"
         />
-      </div>
-      <div className="p-3 text-center">
-        <Link to={`/product/byPath/${path}`}>Products by path</Link>
-        <input
-          type="text"
-          value={path}
-          onChange={(e) => setPath(e.target.value)}
-        />
-      </div>
+          </div>
 
-      <div className="p-3 text-center">
-        <Link to={`/product/topproduct/nn`}>Top Products</Link>
-      </div>
+          <div  style={{
+            position: "absolute",
+            left: "38%",
+            top: "35%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/bypattern/${pattern}`}
+        >By Pattern
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "54%",
+            top: "35%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <TextField
+            type="text"
+            value={pattern}
+            onChange={(e) => setPattern(e.target.value)}
+            required
+            label="Product by Pattern"
+        />
+          </div>
+
+          <div  style={{
+            position: "absolute",
+            left: "39.5%",
+            top: "45%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/similars/${sim}`}
+        >similar cheaper
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "54%",
+            top: "45%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <TextField
+            type="text"
+            value={sim}
+            onChange={(e) => setSim(e.target.value)}
+            required
+            label="Product ID"
+        />
+          </div>
+
+          <div  style={{
+            position: "absolute",
+            left: "40%",
+            top: "55%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/byPath/${path}`}
+        >Products by path
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "54%",
+            top: "55%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+          <TextField
+            type="text"
+            value={path}
+            onChange={(e) => setPath(e.target.value)}
+            required
+            label="Path"
+        />
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "38.5%",
+            top: "65%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/allproduct/nnn`}
+        >All Product
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "38.5%",
+            top: "75%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/categoriestree`}
+        >
+            Categories
+        </Button>
+          </div>
+          <div  style={{
+            position: "absolute",
+            left: "39%",
+            top: "85%",
+            transform: "translate(-50%, -50%)",
+          }}
+          >
+        <Button
+            variant="contained"
+            href={`/product/topproduct/nn`}
+        >Top Products
+        </Button>
+          </div>
     </div>
   );
 }
