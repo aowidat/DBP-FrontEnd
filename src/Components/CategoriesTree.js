74682777da8c7071e175becf5397e516fef8a7d6
aziz <aziz.owidat@gmail.com>
2022-07-06 13:28:11 +0200

    finile

--- src/Components/CategoriesTree.js
+++ src/Components/CategoriesTree.js
@@ -1,40 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import TreeView from '@mui/lab/TreeView';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import TreeItem from '@mui/lab/TreeItem';
-
+import React, { useEffect, useState } from "react";
+import TreeView from "@mui/lab/TreeView";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TreeItem from "@mui/lab/TreeItem";
 
 function CategoriesTree() {
-    const [cat, setCat] = useState();
-    useEffect(() => {
-        fetch(`http://localhost:8080/category/getTreetest`)
-            .then((res) => res.json())
-            .then((data) => setCat(data));
-    }, []);
-    const renderTree = (nodes) => (
-        <TreeItem key={nodes.id} nodeId={nodes.id.toString()} label={nodes.name}>
-            {Array.isArray(nodes.children) && nodes.children.length > 0
-                ? nodes.children.map((node) => renderTree(node))
-                : null}
-        </TreeItem>
-    );
-    return (
-        <TreeView
-            aria-label="controlled"
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpanded={['root']}
-            defaultExpandIcon={<ChevronRightIcon />}
-        >
-            {cat && (() => {
-                let td = [];
-                for (let i = 0; i < cat.length; i++) {
-                    td.push(renderTree(cat[i]));
-                    td.push(<br key={Math.random()} />)
-                }
-                return td;
-            })()}
-        </TreeView>
-    );
+  const [cat, setCat] = useState();
+  useEffect(() => {
+    fetch(`http://localhost:8080/category/getTree`)
+      .then((res) => res.json())
+      .then((data) => setCat(data));
+  }, []);
+  const renderTree = (nodes) => (
+    <TreeItem key={nodes.id} nodeId={nodes.id.toString()} label={nodes.name}>
+      {Array.isArray(nodes.children) && nodes.children.length > 0
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+  return (
+    <TreeView
+      aria-label="controlled"
+      defaultCollapseIcon={<ExpandMoreIcon />}
+      defaultExpanded={["root"]}
+      defaultExpandIcon={<ChevronRightIcon />}
+    >
+      {cat &&
+        (() => {
+          let td = [];
+          for (let i = 0; i < cat.length; i++) {
+            td.push(renderTree(cat[i]));
+            td.push(<br key={Math.random()} />);
+          }
+          return td;
+        })()}
+    </TreeView>
+  );
 }
 export default CategoriesTree;
